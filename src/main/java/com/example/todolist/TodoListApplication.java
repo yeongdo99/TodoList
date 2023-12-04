93ecc7442c7bf4a20d0860446a9d87b23166d4c3
yeongdo99 <atpsfka1@gmail.com>
2023-12-04 11:52:26 +0900

    init commit

---
+++ src/main/java/com/example/todolist/TodoListApplication.java
@@ -0,0 +1,15 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+}
