232c5ec94991fa4b25c4b94fc191e46d52353843
yeongdo99 <atpsfka1@gmail.com>
2023-12-04 11:50:40 +0900

    WebSecurity 설정 및 구현

---
+++ src/main/java/com/example/todolist/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.todolist.security;
+
+import com.example.todolist.user.entity.User;
+import com.example.todolist.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
