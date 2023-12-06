4db7eb12368541c7ce69049eae4bb4776761637d
yeongdo99 <atpsfka1@gmail.com>
2023-12-06 16:52:10 +0900

    Security 구현

--- src/main/java/com/example/todolist/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/todolist/security/UserDetailsServiceImpl.java
@@ -16,7 +16,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findById(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
 
         return new UserDetailsImpl(user);
